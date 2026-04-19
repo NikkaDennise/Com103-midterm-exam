5dec558f4a0bf784b6c784356b959f85c9926e6a
Nikka Dennise Macasling <avocadonikkaniks@gmail.com>
2026-04-19 22:06:23 +0800

    Add files via upload
    Setting up some of the variable

---
+++ Midterm_solutionnn.py
@@ -0,0 +1,9 @@
+Student_name=input("Please enter name: ")
+Weekly_budget=input("Please Enter your Weekly Budget:  ")
+categories = [
+    ["Food & Drinks", "Lunch, snacks, coffee"],
+    ["Transportation", "Bus, jeepney, ride-share"],
+    ["Mobile / Internet", "Load, data plan, WiFi top-up"],
+    ["School Supplies", "Notebook, pen, bond paper"],
+    ["Entertainment", "Games, movies, hangout"]
+]
